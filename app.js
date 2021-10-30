eef4f1959ec2ba2c678323b745a7d3089efb1a96
William Nollert <will.nollert@gmail.com>
2021-10-30 09:50:13 -0500

    test

---
+++ app.js
@@ -0,0 +1,7 @@
+//create 9 squares 
+const firstSquare = document.createElement('div')
+
+firstSquare.innerText = 'hi i am first square'
+
+const app = document.getElementById("app") 
+app.appendChild(firstSquare)
