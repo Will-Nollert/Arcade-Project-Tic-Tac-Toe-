91b016c9d58a8fc3405990aa592cee11ca130713
William Nollert <will.nollert@gmail.com>
2021-10-30 10:38:13 -0500

    added event listeners for boardSquares

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
+let turn = 0
 
 const board = new Array(9).fill(null).map((val, idx) => {
-    return `<div class = "square">${idx + 1 }</div>`
+    return `<div class = "square" id = ${"square-" + (idx + 1 )}></div>`
 });
 
 const app =  document.getElementById("app")
 app.innerHTML = board.join("")
+
+const boardSquares = Array.from(document.querySelectorAll(".square"))
+
+boardSquares.forEach(node => {
+    node.addEventListener ("click", () => {
+        if (node.innerText){
+            return;
+        }
+        let move;
+        if (turn % 2 === 0 ){
+        move = "X"
+        } else {
+        move = "O";
+        }
+        node.innerText = move;
+        turn ++;
+    })
+});
