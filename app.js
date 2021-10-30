0ebcf3f7eaa159fef69ceeef8d92413c273a376c
William Nollert <will.nollert@gmail.com>
2021-10-30 11:30:54 -0500

    started didWin function

--- app.js
+++ app.js
@@ -9,6 +9,17 @@ app.innerHTML = board.join("")
 
 const boardSquares = Array.from(document.querySelectorAll(".square"))
 
+boardSquares.forEach((node, idx) => {
+
+    if (idx % 3 === 0){
+        node.classList.add("column1")
+    } else if ((idx, + 1) % 3 === 0) {
+        node.classList.add("column 3")
+    } else {
+        node.classList.add("column 2")
+    }
+})
+
 boardSquares.forEach(node => {
     node.addEventListener ("click", () => {
         if (node.innerText){
@@ -24,3 +35,29 @@ boardSquares.forEach(node => {
         turn ++;
     })
 });
+
+function didWin(){
+
+    const rows = [
+        [...boardSquares.slice(0, 3)],
+        [...boardSquares.slice(3, 6)],
+        [...boardSquares.slice(6)],
+    ]
+
+    const column1 =Array.fromt(document.querySelectorAll("column1")) 
+    const column2 =Array.fromt(document.querySelectorAll("column2"))
+    const column3 =Array.fromt(document.querySelectorAll("column3"))
+
+    const columns = [column1, column2, column3];
+
+    const diagonal1 = Array.from(
+        document.querySelectorAll("square1, square5, square9")
+    );
+
+    const diagonal2 = Array.from(
+            document.querySelectorAll("square3, square5, square7")
+    );
+
+    const diagonals = [diagonal1, diagonal2];
+
+}
